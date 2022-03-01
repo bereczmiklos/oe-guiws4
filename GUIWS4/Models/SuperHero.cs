f911c5f3134e269c12a4a681d74e8e152c6f5236
Berecz Miklós <bereczmiklos@stud.uni-obuda.hu>
2022-03-01 14:08:34 +0100

    Model class and enum added

--- GUIWS4/Models/SuperHero.cs
+++ GUIWS4/Models/SuperHero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,40 @@ using System.Threading.Tasks;
 
 namespace GUIWS4.Models
 {
-    public class SuperHero
+    public class SuperHero : ObservableObject
     {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { SetProperty(ref speed, value); }
+        }
+
+        public SuperHero GetCopy()
+        {
+            return new SuperHero()
+            {
+                Name = this.Name,
+                Power = this.Power,
+                Speed = this.Speed
+            };
+        }
     }
 }
